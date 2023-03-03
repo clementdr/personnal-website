bf5ca13ee8fbef321e3c2cb5e257f56abf6973da
clementdr <cderoberti@gmail.com>
2023-03-03 12:43:51 +0100

    skeleton initialization

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css" />
+    <link rel="icon" href="#">
+    <title>Accueil</title>
+</head>
+
+<body>
+    <?php
+    include('header.php');
+    ?>
+
+    <main>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam, unde.</p>
+    </main>
+
+    <?php
+    include('footer.php');
+    ?>
+</body>
+
+</html>
